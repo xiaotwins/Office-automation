7c2c02b3afc743efab0fca370d128c1ac952a772
wmj998 <wmj998@foxmail.com>
2021-09-21 17:13:43 +0800

    Update code

--- Word的批量操作/code.py
+++ Word的批量操作/code.py
@@ -1,10 +1,14 @@
+import os
+
 import docx
 import openpyxl
 
-book = openpyxl.load_workbook("D:/文件丶/名单.xlsx")
+book = openpyxl.load_workbook("data/名单.xlsx")
 sheet = book["Sheet1"]
-word = docx.Document("D:/文件丶/模板.docx")
+word = docx.Document("data/模板.docx")
 for i in range(2, 5):
     name = sheet[f"A{i}"].value
     word.paragraphs[0].runs[0].text = name  # 需要修改的位置
-    word.save(f"D:/文件丶/{name}.docx")
+    if not os.path.exists('temp'):
+        os.mkdir('temp')
+    word.save(f"temp/{name}.docx")
--- 文件夹操作/创建文件夹.py
+++ 文件夹操作/创建文件夹.py
@@ -4,9 +4,10 @@ import os
 def mkdir(path):
         folder = os.path.exists(path)
         if not folder:
-            os.mkdir(path)
+            # os.mkdir(path)
+            os.makedirs(path)  # 递归创建目录
 
 
-for i in range(2, 10):
-    path = f'D:\File\GitHub\Data-analysis-and-Application\chapter{i}'
+for i in range(1, 5):
+    path = f'temp/chapter{i}'
     mkdir(path)
